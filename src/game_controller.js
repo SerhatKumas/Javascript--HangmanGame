c2b7e1617862793de6c02bebfb8ee092f944dcba
Serhat Kumas <serhatkumas@outlook.com>
2023-02-01 10:49:21 +0300

    Letter replacement is improved

--- src/game_controller.js
+++ src/game_controller.js
@@ -1,10 +1,12 @@
-letter_guess_count = 0 ;
+let guess_count = 0 ;
+let game_winner_count = 0;
+let randomNumber = 0 ;
 
 word = ["car", "bus", "ferry", "train", "metro"];
 word_explanation = ["Special transportation vehicle", "Public transportation vehicle", "Public transportation used in sea", "Public transportation used between cities", "Public transportation used in city"];
 
 function initiateGame(){
-let randomNumber = getRandomInt(word.length);
+randomNumber = getRandomInt(word.length);
 let p = document.createElement("p");
 p.innerHTML = word_explanation[randomNumber].toUpperCase();
 document.getElementById("word-explanation").appendChild(p);
@@ -15,19 +17,40 @@ for(let i = 0; i<word[randomNumber].length; i++){
     letterDiv.setAttribute("id", "letter-"+i);
     document.getElementById("game-plot").appendChild(letterDiv);
 }
-
 }
 
 function guessLetter(){
     letter_guess = document.getElementById("letter-guess").value;
     document.getElementById("letter-guess").value = "";
-    chechkGame(letter_guess);
+    placeTheLetters(letter_guess);
 }
 
-function chechkGame(guess){
-    console.log(guess);
-    //Guessing letter and placing in the word game
+function placeTheLetters(guess){
+    if(word[randomNumber].split('').indexOf(guess) > -1){
+        let letter_array = letterChecker(guess);
+        letter_array.forEach(element => {
+            document.getElementById("letter-"+element).innerHTML = guess.toUpperCase();
+            game_winner_count++;
+        });
+
+    }
+    else{
+        
+    }
 }
+
+
+function letterChecker(guess){
+let number_counter = []
+let word_array = word[randomNumber].split("");
+for(let i =0; i<word_array.length; i++){
+    if(word_array[i]==guess){
+        number_counter.push(i)
+    }
+}
+return number_counter
+}
+
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
